71abfa176aea16cf73ecf48abb62da3f1ecaea80
Annie Yao <50132471+qyyao@users.noreply.github.com>
2021-02-20 15:05:57 -0800

    initial registration page

--- app/src/main/java/com/example/stormhacks2021/MainActivity.java
+++ app/src/main/java/com/example/stormhacks2021/MainActivity.java
@@ -10,11 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hello - dustin jorgensen
-
-        System.out.println("Hello Val");
-
     }
-
-    //this is Annie :)
 }
