cf7baaeda4f767c520a79328a140ace814648f7b
Adamas8 <valerie.k.1480@gmail.com>
2021-02-20 15:33:12 -0800

    Create User, Senior and CareGiver classes

--- app/src/main/java/com/example/stormhacks2021/MainActivity.java
+++ app/src/main/java/com/example/stormhacks2021/MainActivity.java
@@ -6,15 +6,24 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AppManager appManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hello - dustin jorgensen
 
-        System.out.println("Hello Val");
+        appManager = AppManager.getInstance();
+
+        CareGiver sue = new CareGiver("Sue", "Lan");
+        System.out.println(sue.getID_num());
+        Senior s1 = new Senior("Johnny", "Swan");
+        Senior s2 = new Senior("Bob", "Dylan");
+        Senior s3 = new Senior("Allie", "Johansen");
+        sue.addSenior(s1);
+        sue.addSenior(s2);
+        sue.addSenior(s3);
+        sue.printSeniorsInformation();
 
     }
-
-    //this is Annie :)
 }
