3f7d8967579aa7c9f0e40c9af9eca01f5a7d1011
Adamas8 <valerie.k.1480@gmail.com>
2021-02-21 00:54:15 -0800

    Fix UI and add error check

--- app/src/main/java/com/example/stormhacks2021/Home.java
+++ app/src/main/java/com/example/stormhacks2021/Home.java
@@ -1,19 +1,14 @@
 package com.example.stormhacks2021;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
-
 import com.example.stormhacks2021.homepage.ContactActivity;
 import com.example.stormhacks2021.homepage.HelpActivity;
 import com.example.stormhacks2021.homepage.MedicationActivity;
-import com.example.stormhacks2021.homepage.SettingsActivity;
 
 public class Home extends AppCompatActivity {
 
@@ -25,7 +20,7 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //TO DO: SET THAT IF USER HAS NO PROFILE, SHOW REGISTRATION PAGE
-//        Boolean isFirstRun = //CHECK IF PROFILE EXISTS
+        //Boolean isFirstRun = //CHECK IF PROFILE EXISTS
         appManager = AppManager.getInstance();
         if (appManager.isFirstRun()) {
           //show sign up activity
--- app/src/main/java/com/example/stormhacks2021/SeniorRegistrationPage.java
+++ app/src/main/java/com/example/stormhacks2021/SeniorRegistrationPage.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +13,11 @@ import android.widget.EditText;
 public class SeniorRegistrationPage extends AppCompatActivity {
 
     AppManager appManager;
+    EditText age;
+    EditText lastName;
+    EditText firstName;
+    private boolean isValidText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,21 +25,52 @@ public class SeniorRegistrationPage extends AppCompatActivity {
 
         appManager = AppManager.getInstance();
 
-        EditText firstName = findViewById(R.id.registration_senior_first_name_text);
-        EditText lastName = findViewById(R.id.registration_senior_last_name_text);
-
+        firstName = findViewById(R.id.registration_senior_first_name_text);
+        lastName = findViewById(R.id.registration_senior_last_name_text);
+        age = findViewById(R.id.registration_senior_age_txt);
+        
         Button submit = findViewById(R.id.registration_senior_submit_btn);
+
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String firstNameString = firstName.getText().toString();
-                String lastNameString = lastName.getText().toString();
-                Senior senior = new Senior(firstNameString, lastNameString);
-                appManager.setCurrentSenior(senior);
-                appManager.getSeniors().add(senior);
-                appManager.setFirstRun(false);
-                startActivity(new Intent(SeniorRegistrationPage.this, Home.class));
+                isValidText = validateText();
+                if(isValidText){
+                    String firstNameString = firstName.getText().toString();
+                    String lastNameString = lastName.getText().toString();
+                    String ageString = age.getText().toString();
+                    Senior senior = new Senior(firstNameString, lastNameString);
+                    senior.setAge(Integer.valueOf(ageString));
+                    appManager.setCurrentSenior(senior);
+                    appManager.getSeniors().add(senior);
+                    appManager.setFirstRun(false);
+                    startActivity(new Intent(SeniorRegistrationPage.this, Home.class));
+                }
             }
         });
     }
+
+    private boolean validateText() {
+
+        String firstNameString = firstName.getText().toString();
+        String lastNameString = lastName.getText().toString();
+        String ageString = age.getText().toString();
+        boolean canContinue = true;
+
+        if(firstNameString.length() == 0){
+            firstName.setError("Enter first name");
+            canContinue = false;
+        }
+        if(lastNameString.length() == 0){
+            lastName.setError("Enter last name");
+            canContinue = false;
+        }
+        if(ageString.length() == 0){
+            age.setError("Enter age");
+            canContinue = false;
+        }
+
+        return canContinue;
+    }
+
 }
