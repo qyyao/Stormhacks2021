89264b113c6a4cf86530c0f3cac02e30dee2c171
Warren Ho Kin <whokin@sfu.ca>
2021-02-21 12:07:19 -0800

    Added Logo and App Name

--- app/src/main/java/com/example/stormhacks2021/Home.java
+++ app/src/main/java/com/example/stormhacks2021/Home.java
@@ -158,8 +158,7 @@ public class Home extends AppCompatActivity {
                 alertDialog.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        moveTaskToBack(false);
-                        finish();
+                        dialog.dismiss();
                     }
                 });
                 alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
